a11e0301a16690e9ddb6262ae4a8c4d4aba81a0a
tsalvia <almtr.ctf@gmail.com>
2019-05-20 04:13:46 +0900

    [Add] 統計情報を出力するようにした

--- evtxdump.go
+++ evtxdump.go
@@ -8,10 +8,22 @@ import (
 	"github.com/0xrawsec/golang-evtx/evtx"
 )
 
+type EventStats struct {
+	Channel string
+	EventID int64
+	Count uint
+}
+
 func getFileNameWithoutExt(path string) string {
 	return filepath.Base(path[:len(path) - len(filepath.Ext(path))])
 }
 
+func showStats(stats []EventStats) {
+	fmt.Println("Channel, EventID, Count")
+	for _, s := range stats {
+		fmt.Printf("%s,\t%d,\t%d\n", s.Channel, s.EventID, s.Count)
+	}
+}
 
 func main() {
 	flag.Usage = func() {
@@ -19,9 +31,9 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
-
 	flag.Parse()
 
+	stats := []EventStats{}
 	for _, evtxFile := range flag.Args() {
 		ef, err := evtx.New(evtxFile)
 		if err != nil {
@@ -35,15 +47,27 @@ func main() {
 			fmt.Println("Error:", err)
 			continue
 		}
+		defer of.Close()
 
+		// Begin JSON
 		fmt.Fprintln(of, "[")
-
 		for e := range ef.FastEvents() {
+			contains := false
+			for i, s := range stats {
+				if s.EventID == e.EventID() {
+					stats[i].Count++
+					contains = true
+					break
+				}
+			}
+			if !contains {
+				newStats := EventStats{e.Channel(), e.EventID(), 1}
+				stats = append(stats, newStats)
+			}
 			fmt.Fprintf(of, "\t%s,\n", string(evtx.ToJSON(e)))
 		}
-
-		fmt.Fprintln(of, "\t{}\n]")
-		of.Close()
-
+		fmt.Fprintf(of, "\t{}\n]")
+		// End JSON
 	}
+	showStats(stats)
 }
